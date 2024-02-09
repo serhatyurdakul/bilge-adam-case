98d4f9ee929cd8b6ebc8566139bdb77c11feb7cf
Serhat Yurdakul <serhatyurdakulsl@gmail.com>
2024-02-09 13:04:28 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# bilge-adam-case
+Bilge Adam case, HTML, CSS, JavaScript - Responsive - Mobile Compatible
---
+++ app.js
@@ -0,0 +1,116 @@
+let keywords = [
+  "Adana",
+  "Adıyaman",
+  "Afyonkarahisar",
+  "Ağrı",
+  "Amasya",
+  "Ankara",
+  "Antalya",
+  "Artvin",
+  "Aydın",
+  "Balıkesir",
+  "Bilecik",
+  "Bingöl",
+  "Bitlis",
+  "Bolu",
+  "Burdur",
+  "Bursa",
+  "Çanakkale",
+  "Çankırı",
+  "Çorum",
+  "Denizli",
+  "Diyarbakır",
+  "Edirne",
+  "Elazığ",
+  "Erzincan",
+  "Erzurum",
+  "Eskişehir",
+  "Gaziantep",
+  "Giresun",
+  "Gümüşhane",
+  "Hakkâri",
+  "Hatay",
+  "Isparta",
+  "İçel(Mersin)",
+  "İstanbul",
+  "İzmir",
+  "Kars",
+  "Kastamonu",
+  "Kayseri",
+  "Kırklareli",
+  "Kırşehir",
+  "Kocaeli",
+  "Konya",
+  "Kütahya",
+  "Malatya",
+  "Manisa",
+  "Kahramanmaraş",
+  "Mardin",
+  "Muğla",
+  "Muş",
+  "Nevşehir",
+  "Niğde",
+  "Ordu",
+  "Rize",
+  "Sakarya",
+  "Samsun",
+  "Siirt",
+  "Sinop",
+  "Sivas",
+  "Tekirdağ",
+  "Tokat",
+  "Trabzon",
+  "Tunceli",
+  "Şanlıurfa",
+  "Uşak",
+  "Van",
+  "Yozgat",
+  "Zonguldak",
+  "Aksaray",
+  "Bayburt",
+  "Karaman",
+  "Kırıkkale",
+  "Batman",
+  "Şırnak",
+  "Bartın",
+  "Ardahan",
+  "Iğdır",
+  "Yalova",
+  "Karabük",
+  "Kilis",
+  "Osmaniye",
+  "Düzce",
+];
+
+const inputBox = document.querySelector(".search-bar");
+const resultsBox = document.querySelector(".results-box");
+
+inputBox.onkeyup = function () {
+  let result = [];
+  let input = inputBox.value;
+  if (input.length) {
+    result = keywords.filter((keyword) => {
+      return keyword
+        .toLocaleLowerCase("tr-TR")
+        .trim()
+        .startsWith(input.toLocaleLowerCase("tr-TR").trim());
+    });
+    console.log("result", result);
+  }
+  display(result);
+  if (!result.length) {
+    resultsBox.innerHTML = "";
+  }
+};
+
+function display(result) {
+  const content = result.map((list) => {
+    return "<li onclick=selectInput(this)>" + list + "</li>";
+  });
+  resultsBox.innerHTML = "<ul>" + content.join("") + "</ul>";
+}
+
+function selectInput(list) {
+  inputBox.value = list.innerHTML;
+  resultsBox.innerHTML = "";
+}
